87bd1d89bab6462f49a82cec3819986ede064b28
elvintj <etahmazov7996@ada.edu.az>
2023-04-14 11:29:41 +0400

    HomePage updated

--- fundraising/src/components/pages/HomePage.js
+++ fundraising/src/components/pages/HomePage.js
@@ -10,15 +10,60 @@ export default function HomePage() {
         <main className="main-page">
             <section className="home">
                 <div className="home-body">
-                    <h1>Uplift Bright Ideas</h1>
-                    <h5>Be the one to change the future with fundchain</h5>
-                    <img src={rocket} className="donate-rocket" alt=""/>
-                    <img src={smoke} className="donate-smoke" alt=""/>
+                    <div className="home-content">
+                        <h1>Uplift Bright Ideas</h1>
+                        <h5>Be the one to change the future with fundchain</h5>
+                        <img src={rocket} className="donate-rocket" alt=""/>
+                        <img src={smoke} className="donate-smoke" alt=""/>
+                    </div>
                     <div className="main-buttons">
                         <a href="/projects"><button type="button">VIEW PROJECTS</button></a>
                         <a href="/create-project"><button type="button">CREATE A PROJECT</button></a>
                     </div>
-                    
+                </div>
+            </section>
+            <section className="home--promotion">
+                <div className="promotion--title">
+                    <span>Fundraising on FundChain takes</span>
+                    <span>just a few minutes</span>  
+                </div>
+                <div className="promotion--sections">
+                    <div>
+                        <div className="icon--section">
+                            <i class="fa-solid fa-hand-holding-medical"></i>    
+                        </div>
+                        <div className='text--section'>
+                            <h4>Give Support</h4>
+                            <span>Help startups succeed with your</span>
+                            <span>donation and gain access to</span>
+                            <span> exclusive rewards, support, and</span>
+                            <span>recognition.</span>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="icon--section">
+                            <i class="fa-solid fa-heart-circle-plus"></i> 
+                        </div>
+                        <div className='text--section'>
+                            <h4>Share Happiness</h4>
+                            <span>Spread joy and make a positive</span>
+                            <span>impact on the world by supporting</span>
+                            <span>innovative startups through your</span>
+                            <span>donation.</span>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="icon--section">
+                            <i class="fa-solid fa-lightbulb"></i>
+                        </div>
+                        <div className='text--section'>
+                            <h4>Inspire Others</h4>
+                            <span>Inspire others to create positive</span>
+                            <span>change by supporting innovative</span>
+                            <span>startups and joining a community</span>
+                            <span>of like-minded individuals.</span>
+                        </div>
+                    </div>
                 </div>
             </section>
         </main>
