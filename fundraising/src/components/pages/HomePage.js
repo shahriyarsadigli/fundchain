2125ec8c4e204186b9eb0e49c386d1cfe3ffcd3a
elvintj <etahmazov7996@ada.edu.az>
2023-04-16 13:48:12 +0400

    Homepage updated

--- fundraising/src/components/pages/HomePage.js
+++ fundraising/src/components/pages/HomePage.js
@@ -1,6 +1,7 @@
 import rocket from '../images/Ethereum-Logo.png';
 import smoke from '../images/smokey.png';
 import Header1 from '../headers/Header1.js'
+import Artwork from '../images/Fundchain-artwork.png'
 import '../style/HomePage.css';
 import React from 'react';
 
@@ -11,12 +12,11 @@ export default function HomePage() {
             <section className="home">
                 <div className="home-body">
                     <div className="home-content">
-                        <h1>Uplift Bright Ideas</h1>
-                        <h5>Be the one to change the future with fundchain</h5>
-                        <img src={rocket} className="donate-rocket" alt=""/>
-                        <img src={smoke} className="donate-smoke" alt=""/>
+                        <h1>FundChain empowers <br></br>the future of startups  with<br></br>secure and transparent fundraising.</h1>
+                        <h5>Join the decentralized revolution of startup funding powered by Ethereum.</h5>
+                        <img src={Artwork} className="home--artwork"/>
                     </div>
-                    <div className="main-buttons">
+                    <div className="main--buttons">
                         <a href="/projects"><button type="button">VIEW PROJECTS</button></a>
                         <a href="/create-project"><button type="button">CREATE A PROJECT</button></a>
                     </div>
